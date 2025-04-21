8bf1a701a65d7019fa1929f4340e28cbb774aa25
Salahuddin Saiet <salahuddin@lowdefy.com>
2025-04-21 13:04:50 +0200

    chore: Update code formatting

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,5 @@
-import * as path from "path";
-import { workspace, ExtensionContext, window, OutputChannel } from "vscode";
+import * as path from 'path';
+import { workspace, ExtensionContext, window, OutputChannel } from 'vscode';
 
 import {
   LanguageClient,
@@ -7,18 +7,16 @@ import {
   ServerOptions,
   TransportKind,
   Trace,
-} from "vscode-languageclient/node";
+} from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
   // The server is implemented in node
-  const serverModule = context.asAbsolutePath(
-    path.join("server", "out", "server.js")
-  );
+  const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
 
   // If the extension is launched in debug mode then the debug server options are used
-  const debugOptions = {execArgv: ["--nolazy", "--inspect=6009"]};
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
   // Otherwise the run options are used
   const serverOptions: ServerOptions = {
     run: { module: serverModule, transport: TransportKind.ipc },
@@ -29,17 +27,17 @@ export function activate(context: ExtensionContext) {
     },
   };
 
-  const outputChannel: OutputChannel = window.createOutputChannel("Lowdefy Language Server");
+  const outputChannel: OutputChannel = window.createOutputChannel('Lowdefy Language Server');
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
     // Register the server for all documents by default
     documentSelector: [
-      { scheme: "file", language: "yaml" },
-      { scheme: "file", language: "yml" }
+      { scheme: 'file', language: 'yaml' },
+      { scheme: 'file', language: 'yml' },
     ],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher("**/*.yaml"),
+      fileEvents: workspace.createFileSystemWatcher('**/*.yaml'),
     },
     outputChannel,
     traceOutputChannel: outputChannel,
@@ -47,15 +45,15 @@ export function activate(context: ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    "lowdefy-language-server",
-    "Lowdefy Language Server",
+    'lowdefy-language-server',
+    'Lowdefy Language Server',
     serverOptions,
-    clientOptions,
+    clientOptions
   );
   client.setTrace(Trace.Verbose);
 
   // Start the client. This will also launch the server
-  outputChannel.appendLine("Starting Lowdefy Language Server");
+  outputChannel.appendLine('Starting Lowdefy Language Server');
   client.start();
 }
 
